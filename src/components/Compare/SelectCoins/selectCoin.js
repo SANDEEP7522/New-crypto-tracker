7c73f5645fd731ae36502cdefb468ed68926f0ec
SANDEEP7522 <159306588+SANDEEP7522@users.noreply.github.com>
2024-10-14 22:55:52 +0530

    seclecter 1 & 2 completed

--- src/components/Compare/SelectCoins/selectCoin.js
+++ src/components/Compare/SelectCoins/selectCoin.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import "./selectCoin.css"
 import { get100Coins } from "../../../functions/get100Coins";
 import MenuItem from "@mui/material/MenuItem";
 import Select from "@mui/material/Select";
@@ -25,10 +26,14 @@ function SelectCoins() {
     },
   };
 
-  const handleCoinChange = (event) => {
+  const handleCoinChange = (event, isCoin2) => {
+    if (isCoin2) {
+      setCrypto2(event.target.value);
+      console.log("coin_id_2", event.target.value);
+    } else {
     setCrypto1(event.target.value);
-    console.log("coin_id", event.target.value);
-    
+    console.log("coin_id)_1", event.target.value);
+      }
   };
 
   useEffect(() => {
@@ -41,12 +46,25 @@ function SelectCoins() {
  }
 
   return (
-    <div>
+    <div className="coin-select">
+      <p>Crypto 1</p>
       <Select
         sx={style}
         value={crypto1}
         label="Crypto1"
-        onChange={handleCoinChange}
+        onChange= {(event) => handleCoinChange(event, false)}
+      >
+     {allCoins.map((coin) => (
+        <MenuItem value={coin.id}>{coin.name}</MenuItem>
+     ))}
+      </Select>
+
+      <p>Crypto 2</p>
+      <Select
+        sx={style}
+        value={crypto2}
+        label="Crypto2"
+        onChange = {(event) => handleCoinChange(event, true)}
       >
      {allCoins.map((coin) => (
         <MenuItem value={coin.id}>{coin.name}</MenuItem>
